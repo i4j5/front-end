1b9259fe62c9d35053fb21cdf64a4791b032b6f5
Aleksandr Sazhin <i4j5@ya.ru>
2016-02-02 20:54:14 +0300

    -

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@ var config = require('../config');
 
 gulp.task('watch', function(){
 
-  gulp.watch(config.coffeescripts.src, ['coffeescripts', 'livereload']);
+  gulp.watch(config.coffeescripts.src, ['coffeescripts']);
   gulp.watch(config.stylus.src, ['stylus']);
   gulp.watch(config.jade.src, ['jade']);
 
